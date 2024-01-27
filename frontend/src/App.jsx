94782399945e9e317056bffc4d0c5a42b4f31d86
Vikas singh varma <Vikashverma3001@gmail.com>
2024-01-27 11:12:47 +0530

    rearrage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import SignIn from './pages/SignIn.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Dashboard from './pages/Dashboard.jsx'
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element = {<Home />} />
-        <Route path='/about' element = {<About />} />
-        <Route path='/sign-in' element = {<SignIn/>} />
-        <Route path='/sign-up' element = {<SignUp/>} />
-        <Route path='/dashboard' element = {<Dashboard/>} />
       </Routes>
     </BrowserRouter>
   )
